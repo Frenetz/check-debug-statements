28e6bdc1da947edde53dfb77be830b85a7514277
Frenetz <104676398+Frenetz@users.noreply.github.com>
2024-10-20 12:43:05 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
-contains_debug_statements() {
-	echo "$1" | grep -q -E 'dd\s*\(|console\.log\s*\('
+contains_dd1_statements() {
+  echo "$1" | grep -q -E ' dd\('
+}
+
+contains_dd2_statements() {
+  echo "$1" | grep -q -E ' @dd\('
+}
+
+contains_consolelog_statements() {
+  echo "$1" | grep -q -E ' console\.log\('
 }
 
 git config --global credential.helper "store --file=.git/credentials"
-echo "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com" >.git/credentials
+echo "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com" > .git/credentials
 git config --global --add safe.directory /github/workspace
 
 if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
@@ -19,7 +27,7 @@ git fetch origin
 
 changed_files=$(git diff --name-only "origin/$BASE_BRANCH" "$GITHUB_SHA")
 
-IFS=',' read -r -a exclude_files <<<"$EXCLUDE_FILES"
+IFS=',' read -r -a exclude_files <<< "$EXCLUDE_FILES"
 
 need_throw_error=false
 for filename in $changed_files; do
@@ -31,16 +39,16 @@ for filename in $changed_files; do
 			line_number=0
 			while IFS= read -r line; do
 				line_number=$((line_number + 1))
-				if contains_debug_statements "$line"; then
-					echo "File ${filename}:${line_number} Line \"$line\" contains debug statements (dd or console.log)"
+				if contains_dd1_statements "$line" || contains_dd2_statements "$line" || contains_consolelog_statements "$line"; then
+					echo "File ${filename}:${line_number} Line \"$line\" contains debug statements (dd or @dd or console.log)"
 					need_throw_error=true
 				fi
-			done <"$filename"
+			done < "$filename"
 		fi
 	fi
 done
 
 if $need_throw_error; then
-	echo "Find files with debug statements."
+	echo "Found files with debug statements."
 	exit 1
 fi
